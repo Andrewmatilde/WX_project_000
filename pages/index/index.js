99c91247de77d8230ed6f39e89cd4f6298a9a871
Andrewmatilde <davis6813585854063@outlook.com>
2019-06-19 22:17:07 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,82 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    pageone: true
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  tapName(event, ownerInstance) {
+    var that = this;
+    console.log('tap wechat')
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        wx.getFileSystemManager().readFile({
+          filePath: res.tempFilePaths[0],
+          encoding: 'base64',
+          success: res => {
+            wx.request({
+              url:'https://aip.baidubce.com/rest/2.0/ocr/v1/accurateaccess_token=24.347c8a7fd341c5d878d089fa2b8ab72e.2592000.1563539776.282335-16567079&Content-Type=application/x-www-form-urlencoded',
+              method:"POST",
+              data: {
+                image:res.data
+              },
+              success: function (res) {
+                console.log(res)
+              }
+            })
+          }
+        })
+      }
+    })
+  }
+})
